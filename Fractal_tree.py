c6babbef6fc630df78ec55c61bb19a834d91e4f1
Pratima Yadav <92806716+amitarp@users.noreply.github.com>
2021-12-26 17:02:46 +0530

    Add files via upload

---
+++ Fractal_tree.py
@@ -0,0 +1,26 @@
+import turtle
+
+#creating the object
+hr=turtle.Turtle()
+
+hr.left(90)
+hr.speed(150)
+
+#defining the recursion fuction
+def tree(i):
+    if i<10:
+        
+        return
+    else:
+        hr.forward(i)
+        hr.left(30)
+        tree(3*i/4)
+        hr.right(60)
+        tree(3*i/4)
+        hr.left(30)
+        hr.backward(i)
+
+tree(100)
+
+turtle.done()
+
